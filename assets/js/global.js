d808c14248702434e7cae622c4d3491fde573cbe
hellojoyworldz <hellojoyworldz@gmail.com>
2022-12-03 21:35:36 +0900

    22.12.03 21:35

--- assets/js/global.js
+++ assets/js/global.js
@@ -90,36 +90,3 @@ window.addEventListener("load", function () {
 document.addEventListener("load", () => {
   setTimeout(() => {}, 1000);
 });
-
-imagesProgress();
-function imagesProgress() {
-  let preload = document.querySelector(".preload");
-  progressText = document.querySelector(".progress-text");
-  (imgLoad = imagesLoaded("body")),
-    (imgTotal = imgLoad.images.length),
-    (imgLoaded = 0),
-    (current = 0),
-    (progressTimer = setInterval(updateProgress, 1000 / 60));
-
-  imgLoad.on("progress", function () {
-    imgLoaded++;
-  });
-
-  function updateProgress() {
-    let target = (imgLoaded / imgTotal) * 100;
-
-    current += (target - current) * 0.1;
-    progressText.text(Math.floor(current) + "%");
-
-    if (current >= 100) {
-      clearInterval(progressTimer);
-      preload.addClass("loaded");
-      progressText.delay(1000).animate({ opacity: 0 }, function () {
-        preload.fadeOut();
-      });
-    }
-    if (current > 99.9) {
-      current = 100;
-    }
-  }
-}
