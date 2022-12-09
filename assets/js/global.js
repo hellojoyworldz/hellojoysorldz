ff4adde4b2b9f5bd719e9260259a86f650cecc64
hellojoyworldz <hellojoyworldz@gmail.com>
2022-12-09 23:40:21 +0900

    22.12.09 23:40

--- assets/js/global.js
+++ assets/js/global.js
@@ -85,8 +85,44 @@ window.addEventListener("load", function () {
 
   ScrollTrigger.addEventListener("refresh", () => scroller.update());
   ScrollTrigger.refresh();
+
+  //loading();
 });
 
 document.addEventListener("load", () => {
   setTimeout(() => {}, 1000);
 });
+
+//로딩소스
+function loading() {
+  const header = document.querySelector("#header");
+  let loader = document.querySelector(".loading");
+  let loaderBar = document.querySelector(".loading__bar");
+  let loaderText = document.querySelector(".loading__text");
+  let num = 0;
+  let interval = setInterval(progress);
+
+  let mainText = document.querySelector(".visual__tit");
+  let mainText2 = document.querySelector(".visual__sbj--move");
+  let mainImg = document.querySelector(".visual__emoji");
+
+  imagesLoaded(document.querySelector("html"), interval);
+
+  function progress() {
+    num++;
+    loaderBar.style.width = num + "%";
+    loaderText.innerText = num + "%";
+
+    if (num === 100) {
+      clearInterval(interval);
+      setTimeout(() => {
+        loader.style.display = "none";
+
+        gsap.from(header, { duration: 0.8, opacity: 0, delay: 0.1 });
+        gsap.from(mainText, { duration: 0.8, y: -20, opacity: 0, delay: 0.4 });
+        gsap.from(mainText2, { duration: 1.8, x: 20, opacity: 0, delay: 0.6 });
+        gsap.from(mainImg, { duration: 1.5, opacity: 0, delay: 0.8 });
+      }, 500);
+    }
+  }
+}
