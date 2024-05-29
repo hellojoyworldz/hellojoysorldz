ce7e8f044c2ac39a244824bda0cc9a2a3867d84a
hellojoyworldz <hellojoyworldz@gmail.com>
2024-05-29 20:12:49 +0900

    [PORTFOLIO-5] router, react-query 세팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./assets/styles/reset.scss";
+import { Routes, Route } from "react-router-dom";
+import HomepageLayout from "./Layout/HomepageLayout/HomepageLayout";
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomepageLayout />}>
+        <Route index element={<Homepage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/Layout/HomepageLayout/HomepageLayout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+const HomepageLayout = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default HomepageLayout;
