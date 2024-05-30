2ec08a7c8e4cdf2fd701df9b12a1ec93ef3df7f1
hellojoyworldz <hellojoyworldz@gmail.com>
2024-05-31 01:31:11 +0900

    [PORTFOLIO-6] Homepage - 페이지 구성

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,27 @@
+import Visual from "./components/Visual/Visual";
+import Intro from "./components/Intro/Intro";
+import SiteProject from "./components/SiteProject/SiteProject";
+import SitePublishing from "./components/SitePublishing/SitePublishing";
+import Banner from "./components/Banner/Banner";
+import SiteCss from "./components/SiteCss/SiteCss";
+import SiteJavaScript from "./components/SiteJavaScript/SiteJavaScript";
+import Profile from "./components/Profile/Profile";
+
 const Homepage = () => {
-  return <>Homepage</>;
+  return (
+    <div id="contents">
+      <main className="main">
+        <Visual />
+        <Intro />
+        <SiteProject />
+        <SitePublishing />
+        <Banner />
+        <SiteCss />
+        <SiteJavaScript />
+        <Profile />
+      </main>
+    </div>
+  );
 };
 
 export default Homepage;
