990c99ce21c990b7522e78e14693d184d52b06dc
hellojoyworldz <hellojoyworldz@gmail.com>
2024-06-01 01:07:55 +0900

    [PORTFOLIO-7] Profile Component 수정

--- src/pages/Homepage/components/Profile/Profile.jsx
+++ src/pages/Homepage/components/Profile/Profile.jsx
@@ -1,9 +1,26 @@
 import "./Profile.style.scss";
 import photo from "../../../../assets/images/IMG_0419.JPG";
-const Profile = () => {
+
+const data = {
+  textpath:
+    "HTML • CSS • jQuery • JavaScript • React • Vue • HTML • CSS • jQuery • JavaScript • React • Vue • HTML • CSS • jQuery • JavaScript • React • Vue • HTML • CSS • jQuery • JavaScript • React • Vue ",
+  footersbj:
+    "IMPOSSIBLE IS NOTING · IMPOSSIBLE IS NOTING · IMPOSSIBLE IS NOTING · IMPOSSIBLE IS NOTING · IMPOSSIBLE IS NOTING · IMPOSSIBLE IS NOTING · IMPOSSIBLE IS NOTING ·",
+  footerdesc: "더 좋은 결과를 위해 계속 수정 중에 있습니다",
+  info: {
+    name: "Gwak Sunah",
+    email: "hellojoyworldz@gmail.com",
+    phone: "+82 10-9783-6375",
+    portfolio: "hellojoyworldz.github.io/myPORTFOLIO",
+    github: "github.com/hellojoyworldz",
+    blog: "hellojoyworldz.tistory.com",
+  },
+};
+
+const Profile = ({ bgcolor }) => {
   return (
     <>
-      <section className="mainProfile" data-bgcolor="#ff7896">
+      <section className="mainProfile" data-bgcolor={bgcolor}>
         <svg
           className="mainProfile__svg"
           width="1440"
@@ -15,7 +32,7 @@ const Profile = () => {
           <path
             d="M-17.4829 15.9294C297.2 -24.1211 339.25 187.141 393.211 351.93C447.171 516.719 617.113 575.553 790.539 486.617C963.965 397.68 1095.42 189.366 1488.7 319.17"
             stroke="#FF7896"
-            stroke-width="50"
+            strokeWidth="50"
             id="text-path"
           ></path>
           <text dy="1.5%" y="15px">
@@ -24,14 +41,7 @@ const Profile = () => {
               fill="currentColor"
               startOffset="-197.5816"
             >
-              HTML • CSS • jQuery • JavaScript • React • Vue • HTML • CSS •
-              jQuery • JavaScript • React • Vue • HTML • CSS • jQuery •
-              JavaScript • React • Vue • HTML • CSS • jQuery • JavaScript •
-              React • Vue • HTML • CSS • jQuery • JavaScript • React • Vue •
-              HTML • CSS • jQuery • JavaScript • React • Vue • HTML • CSS •
-              jQuery • JavaScript • React • Vue • HTML • CSS • jQuery •
-              JavaScript • React • Vue • HTML • CSS • jQuery • JavaScript •
-              React • Vue • HTML • CSS • jQuery • JavaScript • React • Vue
+              {data.textpath}
               <animate
                 attributeName="startOffset"
                 begint="0"
@@ -43,55 +53,62 @@ const Profile = () => {
             </textPath>
           </text>
         </svg>
-        <span className="mainProfile__thumb">
-          <img src={photo} alt="" />
-        </span>
+        {photo && (
+          <span className="mainProfile__thumb">
+            <img src={photo} alt="" />
+          </span>
+        )}
 
         <footer className="footer" data-bgcolor="#ff7896">
           <h2 className="screenHide">CONTACT ME</h2>
           <address className="footer__info">
-            <div className="footer__desc">
-              "더 좋은 결과를 위해 계속 수정 중에 있습니다"
-            </div>
+            {data.footerdesc && (
+              <div className="footer__desc">{data.footerdesc}</div>
+            )}
             <ul className="list">
-              <li className="list__item">
-                <strong className="list__tit">NAME</strong>Gwak Sunah
-              </li>
-              <li className="list__item">
-                <strong className="list__tit">E-mail</strong>
-                <a href="toemail:hellojoyworldz@gmail.com">
-                  hellojoyworldz@gmail.com
-                </a>
-              </li>
-              <li className="list__item">
-                <strong className="list__tit">Phone</strong>
-                <a href="SMS:010-9783-6375">+82 10-9783-6375</a>
-              </li>
-              <li className="list__item">
-                <strong className="list__tit">Portfolio</strong>
-                <a href="https://hellojoyworldz.github.io/myPORTFOLIO/">
-                  hellojoyworldz.github.io/myPORTFOLIO
-                </a>
-              </li>
-              <li className="list__item">
-                <strong className="list__tit">Git-Hub</strong>
-                <a href="https://github.com/hellojoyworldz" target="_blank">
-                  github.com/hellojoyworldz
-                </a>
-              </li>
-              <li className="list__item">
-                <strong className="list__tit">Blog</strong>
-                <a href="https://hellojoyworldz.tistory.com/" target="_blank">
-                  hellojoyworldz.tistory.com
-                </a>
-              </li>
+              {data.info.name && (
+                <li className="list__item">
+                  <strong className="list__tit">Name</strong>
+                  {data.info.name}
+                </li>
+              )}
+              {data.info.email && (
+                <li className="list__item">
+                  <strong className="list__tit">E-mail</strong>
+                  <a href={data.info.email}>{data.info.email}</a>
+                </li>
+              )}
+              {data.info.phone && (
+                <li className="list__item">
+                  <strong className="list__tit">Phone</strong>
+                  <a href={`SMS:${data.info.phone}`}>{data.info.phone}</a>
+                </li>
+              )}
+              {data.info.portfolio && (
+                <li className="list__item">
+                  <strong className="list__tit">Portfolio</strong>
+                  <a href={data.info.portfolio}>{data.info.portfolio}</a>
+                </li>
+              )}
+              {data.info.github && (
+                <li className="list__item">
+                  <strong className="list__tit">Git-Hub</strong>
+                  <a href={data.info.github} target="_blank">
+                    {data.info.github}
+                  </a>
+                </li>
+              )}
+              {data.info.blog && (
+                <li className="list__item">
+                  <strong className="list__tit">Blog</strong>
+                  <a href={data.info.blog} target="_blank">
+                    {data.info.blog}
+                  </a>
+                </li>
+              )}
             </ul>
           </address>
-          <span className="footer__sbj">
-            IMPOSSIBLE IS NOTING · IMPOSSIBLE IS NOTING · IMPOSSIBLE IS NOTING ·
-            IMPOSSIBLE IS NOTING · IMPOSSIBLE IS NOTING · IMPOSSIBLE IS NOTING ·
-            IMPOSSIBLE IS NOTING · IMPOSSIBLE IS NOTING ·
-          </span>
+          <span className="footer__sbj">{data.footersbj}</span>
         </footer>
       </section>
     </>
